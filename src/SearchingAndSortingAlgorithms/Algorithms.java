70f84ab0ef6f68e413ce60aca044a1b973e418b0
tlcrack <league@WTS7.attlocal.net>
2018-03-19 20:00:20 -0700

    3/19/18

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -2,6 +2,10 @@ package SearchingAndSortingAlgorithms;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Algorithms {
@@ -84,25 +88,72 @@ public class Algorithms {
 		return false;
 	}
 
-//	public static List<Double> sortScores(List<Double> results) {
-//		double holder = 0;
-//		for(int i = 0; i < results.size()-1; i++) {
-//			
-//			if(results.get(i)>results.get(i+1)) {
-//				holder = results.get(i);
-//				results.set(i, results.get(i+1));
-//				results.set(i+1, holder);
-//			}
-//			
-//			
-//		}
-//		
-//		
-//		
-//		
-//		return results;
-//	}
+	public static List<Double> sortScores(List<Double> results) {
+		
+		boolean sort = true;
+		while(sort) {
+			sort = false;
+		double holder = 0;
+		for(int i = 0; i < results.size()-1; i++) {
+			
+			if(results.get(i)>results.get(i+1)) {
+				holder = results.get(i);
+				results.set(i, results.get(i+1));
+				results.set(i+1, holder);
+				sort = true;
+			}			
+			
+		}
 	
-	
+		
+		}
+		
+		return results;
+	}
 	//Add other methods here
+
+	public static Object sortDNA(List<String> unsortedSequences) {
+		
+		List<String> sorted = new ArrayList<String>();
+		HashMap<Integer, String> list = new HashMap<Integer, String>();
+		for(int i = 0; i < unsortedSequences.size(); i++) {
+			char[] a = unsortedSequences.get(i).toCharArray();
+			list.put(a.length, unsortedSequences.get(i));
+		}
+		
+		Object[] keys = list.keySet().toArray();
+		Arrays.sort(keys);
+		
+		for(int j = 0; j < unsortedSequences.size(); j++) {
+			sorted.add(list.get(keys[j]));
+		}
+		
+		
+		
+		return sorted;
+		
+	}
+
+	public static List<String> sortWords(List<String> words) {
+		
+		boolean sort = true;
+		while(sort) {
+			sort = false;
+		for(int i = 0; i < words.size()-1; i++) {
+			String holder = "";
+			if(words.get(i).compareTo(words.get(i+1)) == 1) {
+				holder = words.get(i);
+				words.set(i, words.get(i+1));
+				words.set(i+1, holder);
+				sort = true;
+			}
+			
+			
+		}
+		}
+		
+		
+		
+		return words;
+	}
 }
